b0917913708b479576c0915e326c9b5487d8dc4a
Martine Lenders <m.lenders@fu-berlin.de>
2021-02-12 15:27:07 +0100

    Revert "riotctrl: drop python 3.5 testing"
    This reverts commit 4b30a25638fdd5cd2d6e69fa22858b0da99eed35.
    
    Sadly, some of our test environment still runs on Python 3.5, so we need
    to revert this for now.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     classifiers=['Development Status :: 2 - Pre-Alpha',
                  'Programming Language :: Python',
                  'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
@@ -50,5 +51,5 @@ setup(
     extras_require={
         'rapidjson': ['python-rapidjson']
     },
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )

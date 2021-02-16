e848351b13d40f63d7bf11d6becb7fbbeabfd204
Martine Lenders <m.lenders@fu-berlin.de>
2021-02-16 17:31:49 +0100

    riotctrl.ctrl: provide abstract factory and factory for BOARD env

--- riotctrl/ctrl.py
+++ riotctrl/ctrl.py
@@ -3,6 +3,7 @@
 Define class to abstract a node over the RIOT build system.
 """
 
+import abc
 import os
 import time
 import logging
@@ -188,3 +189,67 @@ class RIOTCtrl():
             command.extend(dir_cmd)
         command.extend(targets)
         return command
+
+
+class RIOTCtrlFactoryBase(abc.ABC):
+    # pylint: disable=too-few-public-methods
+    # A factory usually does not have more methods than one.
+    """Abstract factory to create different RIOTCtrl."""
+
+    @abc.abstractmethod
+    def get_ctrl(self, application_directory='.', env=None):
+        """
+        Returns a RIOTCtrl object of a class specified by the Factory
+
+        :param application_directory: `application_directory` initialization
+                                      parameter for the RIOTCtrl object
+        :param env:                   `env` initialization parameter for
+                                      the RIOTCtrl object.
+        """
+        raise NotImplementedError
+
+
+class RIOTCtrlBoardFactory(RIOTCtrlFactoryBase):
+    # pylint: disable=too-few-public-methods
+    # A factory usually does not have more methods than one.
+    """Factory mixin to create different RIOTCtrl types based on
+    the BOARD environment variable.
+
+    :param board_cls: A dict that maps the `BOARD` environment variable to a
+                      RIOTCtrl class.
+    """
+    DEFAULT_CLS = RIOTCtrl
+    BOARD_CLS = {}
+
+    def __init__(self, board_cls=None):
+        self.board_cls = {}
+        self.board_cls.update(self.BOARD_CLS)
+        if board_cls is not None:
+            self.board_cls.update(board_cls)
+
+    def get_ctrl(self, application_directory='.', env=None):
+        """
+        Returns a RIOTCtrl object of a class as specified in `board_cls` on
+        initialization.
+
+        :param application_directory: `application_directory` initialization
+                                      parameter for the RIOTCtrl object
+        :param env:                   `env` initialization parameter for
+                                      the RIOTCtrl object. This will also be
+                                      used to determine the actual class of
+                                      the return value.
+
+        When `BOARD` is set in the environment variables when `env` is provided
+        in `env`, that value is used to look-up the RIOTCtrl class in the
+        factory's `board_cls` for that specific `BOARD` value.
+        """
+        the_env = {}
+        the_env.update(os.environ)
+        if env:
+            the_env.update(env)
+        if 'BOARD' not in the_env or the_env['BOARD'] not in self.board_cls:
+            cls = self.DEFAULT_CLS
+        else:
+            cls = self.board_cls[the_env['BOARD']]
+        # cls does its own fetching of `os.environ` so only provide `env` here
+        return cls(application_directory=application_directory, env=env)
